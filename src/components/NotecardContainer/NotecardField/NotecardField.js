66261e03cef77ff2c56da6ed8be7adbb65de5ab2
kalenai <kalenairedtail@gmail.com>
2018-04-21 22:41:05 -0500

    Setup placeholder text on the NotecardField components

--- src/components/NotecardContainer/NotecardField/NotecardField.js
+++ src/components/NotecardContainer/NotecardField/NotecardField.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ContentEditable from 'react-sane-contenteditable';
+import classes from './NotecardField.css';  
 
 const NotecardField = (props) => {
-  const content = props.content !== "" ? props.content : props.placeholder;
+  const { placeholder, content } = props;
 
   return (
     <ContentEditable
       content={content}
+      className={classes.NotecardField}
+      tagName='span'
       multiLine={true}
       onChange={props.handleChange}
+      placeholder={placeholder}
     />
   );
 };
